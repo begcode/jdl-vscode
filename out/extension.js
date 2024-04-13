68e16f5574807f1dfd1c3c8ccd2bf52f80c94344
wangxin <wangxinxx@163.com>
2024-04-13 14:56:56 +0800

    init

---
+++ out/extension.js
@@ -0,0 +1,21 @@
+"use strict";
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
+function activate(context) {
+    // context.subscriptions.push(
+    // 	vscode.languages.registerHoverProvider('jdl', {
+    // 		provideHover(document, position, token) {
+    // 			log('token: ', token);
+    // 			log('position: ', position);
+    // 			return {
+    // 				contents: ['Hover Content']
+    // 			};
+    // 		},
+    // 	}),
+    // );
+}
+exports.activate = activate;
+//# sourceMappingURL=extension.js.map
---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import { log } from 'console';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	// context.subscriptions.push(
+	// 	vscode.languages.registerHoverProvider('jdl', {
+	// 		provideHover(document, position, token) {
+	// 			log('token: ', token);
+	// 			log('position: ', position);
+	// 			return {
+	// 				contents: ['Hover Content']
+	// 			};
+	// 		},
+	// 	}),
+	// );
+}
